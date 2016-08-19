5d4ecfb643a303b2109dd009be44f6fb5779d7fc
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-08-19 08:54:56 -0300

    minor refactor

--- src/bus.ts
+++ src/bus.ts
@@ -15,8 +15,8 @@ export interface Messenger {
 }
 
 export interface Listener {
-  on(token: string, cb: Function): destroyFn;
-  once(token: string, cb: Function): void;
+  on(token: string, cb: Function):destroyFn;
+  once(token: string, cb: Function):void;
 }
 
 export interface Destroyer {
@@ -30,7 +30,7 @@ export class Bus implements Messenger, Listener, Destroyer {
     this._q = [];
   }
 
-  public emit(token:string, info?:any):void {
+  public emit(token: string, info?: any):void {
     for (let i = 0, len = this._q.length; i < len; i++) {
       if (this._q[i].token === token) {
         this._q[i].cb(info);
@@ -43,7 +43,7 @@ export class Bus implements Messenger, Listener, Destroyer {
     }
   }
 
-  public on(token:string, cb:Function): destroyFn {
+  public on(token: string, cb: Function):destroyFn {
     let _id = this._genId();
 
     this._q.push({
@@ -94,7 +94,7 @@ export class Bus implements Messenger, Listener, Destroyer {
     this._q = this._q.filter((item) => !item.del);
   }
 
-  private _s4(): string {
+  private _s4():string {
     return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   }
 
